1b0d5be3cb0053ddb0290ccc952c5167849ece29
NoeliaToledano <noeliatoledanocampos@hotmail.com>
2024-03-13 16:40:36 +0100

    tercer commit

--- js/button.js
+++ js/button.js
@@ -2,8 +2,10 @@ document.getElementById('ocultarSlide').addEventListener('click', function() {
     // Obtener referencia a la slide
     var slide = document.getElementById('slide');
     var slide2 = document.getElementById('slide2');
+    var myaudio = document.getElementById('myAudio');
     
     // Ocultar la slide cambiando su estilo de visualización
     slide.style.display = 'none';
     slide2.style.display = 'block';
+    myaudio.style.display = 'block';
 });
